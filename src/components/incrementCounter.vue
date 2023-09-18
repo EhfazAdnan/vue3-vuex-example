8c40e55721e2680d21570d44ea034afc7cdaa79e
ehfazadnan <ehfazadnan.cse@gmail.com>
2023-09-18 12:34:06 +0600

    vuex state mutations

---
+++ src/components/incrementCounter.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="increment">Increment</button>
+  <button @click="incrementByNumber">Increment By 10</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    increment(){
+      this.$store.commit('increment')
+    },
+    incrementByNumber(){
+      this.$store.commit('incrementByNumber', 10)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createStore } from 'vuex'
 
-createApp(App).mount('#app')
+const store = createStore({
+    state(){
+        return {
+            counter: 0
+        }
+    },
+    mutations : {
+        increment(state){
+            state.counter++
+        },
+        incrementByNumber(state, payload){
+            state.counter = state.counter + payload
+        }
+    }
+})
+
+createApp(App).use(store).mount('#app')
